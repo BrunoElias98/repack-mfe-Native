803f7a8207d4c4290ca910bfe4d66e997c219c77
brunoelias98 <bruno.elias3312@gmail.com>
2025-02-19 10:22:26 -0300

    feat: added eventListener to handle localStorage

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import {Text, View, Button, StyleSheet} from 'react-native';
-
+import {Text, View, Button, StyleSheet, DeviceEventEmitter} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useStore} from './store';
 
 interface CartProps {
   onCountChange?: (newCount: number) => void;
-  externalCount?: number;
 }
 
 const App = ({onCountChange}: CartProps) => {
@@ -16,10 +15,34 @@ const App = ({onCountChange}: CartProps) => {
     onCountChange?.(count + 1);
   };
 
+  const handleSaveToStorage = async () => {
+    try {
+      await AsyncStorage.setItem(
+        '@shared_storage',
+        JSON.stringify({message: 'Valor salvo pela Cart2 (parcel)!'}),
+      );
+
+      DeviceEventEmitter.emit('storage_updated');
+    } catch (error) {
+      console.error('Erro ao salvar:', error);
+    }
+  };
+
+  const handleClearStorage = async () => {
+    try {
+      await AsyncStorage.removeItem('@shared_storage');
+      DeviceEventEmitter.emit('storage_updated');
+    } catch (error) {
+      console.error('Erro ao limpar storage:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Parcel Native App</Text>
       <Button title="Increment" onPress={handleIncrement} />
+      <Button title="Salvar no Storage" onPress={handleSaveToStorage} />
+      <Button title="Limpar Storage" onPress={handleClearStorage} />
     </View>
   );
 };
